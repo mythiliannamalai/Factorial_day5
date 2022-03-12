4c38fdf4d69f5bd3e416a0154936a510d0009744
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-12 19:09:30 +0530

    Factorial

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Fac_pro
+{
+    class Program
+    {
+        public void Calulation()
+        {
+            int user_enter;
+            int factorial = 1;
+            Console.WriteLine("the value : ");
+            user_enter = int.Parse(Console.ReadLine());
+            for (int i = user_enter; i > 1; i--)
+            {
+                factorial = i * factorial;
+            }
+            Console.WriteLine("factorial value : " + factorial);
+        }
+    }
+    class Result
+    {
+        public static void Main(string[] args)
+        {
+            Program pro = new Program();
+            pro.Calulation();
+        }
+    }
+}
